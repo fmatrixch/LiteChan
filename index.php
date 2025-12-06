83a295d4c19e12df229585c668fd18a7abdabef5
fmatrixch <155826599+fmatrixch@users.noreply.github.com>
2025-12-05 21:51:31 -0500

    Refactor index.php for better readability and updates

--- index.php
+++ index.php
@@ -18,14 +18,14 @@ while($admin=mysqli_fetch_assoc($site)){
 </head>
 <title><?php echo $admin['sitename']; ?></title>
 <h1 style="color:white;background-color:blue;text-align:center;"><?php echo $admin['sitename']; }?></h1>
-<span style="text-align:center;"><a href="/admin">篮圈</a> <a href="https://github.com/fmatrixch/LiteChan">项目</a></span>
-<h3 style="color:white;background-color:blue;text-align:center;">发送串</h3>
-<i>发送串时请遵循 HTML 语法规则。可酌情运用各种节目效果比如</i><span style="color:black;background-color:black;">刮刮乐</span><br>
+<span style="text-align:center;"><a href="https://github.com/fmatrixch/LiteChan">GitHub Project</a></span>
+<h3 style="color:white;background-color:blue;text-align:center;">Threads</h3>
+<i>HTML-styling required. You can</i><span style="color:black;background-color:black;">blackout</span>here!<br>
 <form action="upload.php" method="post">
-名称：<input type="text" name="name" placeholder="不要写得太离谱"><br>
-内容：<input type="text" name="pos" size="50" placeholder="发帖不规范，饼干两行泪"><br>
-Emoji：<br>
-<input type="radio" name="emoji" value=" ">无emoji<br>
+Name: <input type="text" name="name" placeholder="Type here"><br>
+Post: <input type="text" name="pos" size="50" placeholder="Type here"><br>
+Emoji: <br>
+<input type="radio" name="emoji" value=" ">N/A<br>
 <input type="radio" name="emoji" value="/(ㄒoㄒ)/~~">/(ㄒoㄒ)/~~<br>
 <input type="radio" name="emoji" value="O(∩_∩)O">O(∩_∩)O<br>
 <input type="radio" name="emoji" value="(●ˇ∀ˇ●)">(●ˇ∀ˇ●)<br>
@@ -37,9 +37,9 @@ Emoji：<br>
 <input type="radio" name="emoji" value="(*^_^*)">(*^_^*)<br>
 <input type="radio" name="emoji" value="￣へ￣">￣へ￣<br>
 <input type="radio" name="emoji" value="|電柱|">|電柱|<br>
-签名档：<input type="text" name="append" placeholder="随便写"><input type="radio" name="append" value=" ">你可以不带这玩意<br>
-身份：<input type="radio" name="name" value="匿名">以匿名身份发送<br>
-<input type="submit" value="发送">
+Append: <input type="text" name="append" placeholder="Actually"><input type="radio" name="append" value=" ">you can come without this<br>
+Anonymous? <input type="radio" name="name" value="Anonymous">Yes<br>
+<input type="submit" value="Send">
 </form>
 
 
@@ -55,4 +55,5 @@ while($output=mysqli_fetch_assoc($dat)){
 
 ?>
 <br>
+
 <span style="color:grey;">©<?php echo date("Y"); ?> <?php echo $admin['sitename']; ?></span>
